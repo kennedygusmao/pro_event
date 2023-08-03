9ac4b9fbe158cbbb1f4efe4671453e9a56b788b4
ekgdanjos <edemilson.k.anjos@exxonmobil.com>
2023-08-03 15:52:51 -0300

    add test

--- Backend/src/ProEvent.API/WeatherForecast.cs
+++ Backend/src/ProEvent.API/WeatherForecast.cs
@@ -8,5 +8,13 @@ public class WeatherForecast
 
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+    public string Summary { get; set; }
+
+
+    public WeatherForecast(string summary, int temperatureC)
+    {
+        this.Summary = summary;
+        this.TemperatureC = temperatureC;
+
+    }
 }
